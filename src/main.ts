fcd8bc7a6fda996f87104619eb8928806f1e2252
Lars M Bek <lars@beksoft.dk>
2023-09-01 09:43:55 +0200

    Improved code structure

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
 import './style.scss'
-import DOMhead from "../src/wireframe/DOM-with-head.json";
+//import DOMhead from "../src/wireframe/DOM-with-head.json"; // test purposes only - don't use this one, as it will make duplicate <head> tag
 import {HTMLTreeNode} from "./type/types.ts"
 import {Wireframe} from "./Wireframe.ts";
-//import DOM from "../src/wireframe/DOM.json";
+import DOM from "../src/wireframe/DOM.json";
 
 ( ()=>{
     // Create a new Wireframe instance with the 'html' data
-    new Wireframe(DOMhead.html as HTMLTreeNode);
+    new Wireframe(DOM.html as HTMLTreeNode);
 } )();
