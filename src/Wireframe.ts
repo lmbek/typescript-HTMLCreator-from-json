b4dc6ee94f2353bf3dbd9403ab7da932ada126e0
Lars M Bek <lars@beksoft.dk>
2023-09-01 10:07:19 +0200

    Wireframe.ts: Modified * Moved type HTMLTreeNode to this file from type.ts file

--- src/Wireframe.ts
+++ src/Wireframe.ts
@@ -1,23 +1,37 @@
-import {HTMLTreeNode} from "./type/types.ts"
+export type HTMLTreeNode = {
+    tag: string;
+    attributes?: Record<string, string>;
+    innerText?: string;
+    children?: HTMLTreeNode[] | null;
+};
 
+// Wireframe - a function that use data (usually from json) and generate html from the structure of the data
 export class Wireframe {
     constructor(data: HTMLTreeNode) {
         this.insertHTML(document.body, data);
     }
 
     insertHTML(parent: HTMLElement, DOM: HTMLTreeNode) {
+        // for each child we add the attributes, add the innerText and add the children elements
         DOM.children?.forEach((elem: any) => {
+            // this element will be associated with a tag of type HTMLElement
             const element = document.createElement(elem.tag) as HTMLElement;
+
+            // add attributes to this element
             const attributes = elem.attributes as Record<string, string>;
             if(attributes != null){
                 for (const [key, value] of Object.entries(attributes)) {
                     element.setAttribute(key, value);
                 }
             }
+            // add innerText to this element
             if(elem.innerText != null){
                 element.innerText = elem.innerText;
             }
+            // add this (child) element to the parent in html
             parent.appendChild(element);
+
+            // run this method recursively for all children
             this.insertHTML(element, elem);
         });
     }
